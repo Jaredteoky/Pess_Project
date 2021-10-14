c33673500a0d4a1ad68ddd552da31ec5ffcb9869
Jaredteoky <92508772+Jaredteoky@users.noreply.github.com>
2021-10-14 15:21:10 +0800

    First update
    Update of 6 initial files

---
+++ nav.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="navbar.css">
+
+<center><img src="banner.png" width="1060" height="160" alt=""/></center>
+<header>
+<div class ="navbar">
+	
+	<ul>
+    <li><a href="Log call.php">Log Call</a></li>
+    <li><a href="Update.php">Update</a></li>
+    <li><a href="#">Report</a></li>
+    <li><a href="#">History</a></li>
+	</ul>
+</div> 
+	
+</header>
